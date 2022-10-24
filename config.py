c7cac1789b6eeb215310748422bb7fffbb33cb3d
Qobiljon <86218810+MrQobiljon@users.noreply.github.com>
2022-10-24 10:50:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ TOKEN = '5750917770:AAHlM9oJpRU08-YhLgHXxEC4pUIKn6yme2s'
 
 
 
-DB_HOST = 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com'
-DB_NAME = 'd6tm6qgk3rpa1v'
-DB_USER = 'frkejghpwevxjq'
+DB_HOST = 'ec2-54-228-218-84.eu-west-1.compute.amazonaws.com'
+DB_NAME = 'd5evce00bdkbjb'
+DB_USER = 'tososscskygmst'
 DB_PORT = '5432'
-DB_PASSWORD = '236cc8d329abb7c0abd34acc426b8fb51e9e57dcc1ac90567dddf77d332d0406'
+DB_PASSWORD = 'ef523be569370df036c423301f6f8e948c02aba70805dd5b888735e5476e243e'
 # DB_URL = 'postgres://marlqoxtqbeked:57f6111c429d83dc2c962e6301208ed17f0545f275030c7f14519409784229af@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/dav2qck1drejpe'
 # HEROKU_CLI = 'heroku pg:psql postgresql-elliptical-45321 --app convert-word-to-pdf-app'
 
