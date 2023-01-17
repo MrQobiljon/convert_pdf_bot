9e3850b5d5a442ef35746edd47efa76810751147
Qobiljon <86218810+MrQobiljon@users.noreply.github.com>
2023-01-18 01:17:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
-TOKEN = '5750917770:AAHlM9oJpRU08-YhLgHXxEC4pUIKn6yme2s'
-
-# DB_USER = 'postgres'
-# DB_HOST = 'localhost'
-# DB_PASSWORD = '123456'
-# DB_NAME = 'convert_to_pdf'
-# DB_PORT = '5432'
+TOKEN = ''
 
 
+DB_HOST = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PORT = ''
+DB_PASSWORD = ''
 
-DB_HOST = 'ec2-54-228-218-84.eu-west-1.compute.amazonaws.com'
-DB_NAME = 'd5evce00bdkbjb'
-DB_USER = 'tososscskygmst'
-DB_PORT = '5432'
-DB_PASSWORD = 'ef523be569370df036c423301f6f8e948c02aba70805dd5b888735e5476e243e'
-# DB_URL = 'postgres://marlqoxtqbeked:57f6111c429d83dc2c962e6301208ed17f0545f275030c7f14519409784229af@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/dav2qck1drejpe'
-# HEROKU_CLI = 'heroku pg:psql postgresql-elliptical-45321 --app convert-word-to-pdf-app'
-
-ADMINS = [1490953190, 1458590037]
+ADMINS = []
